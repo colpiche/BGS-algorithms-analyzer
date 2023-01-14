c6f7f45fb2963485bdcaaf1a9c61bd569b371bbb
Colin BERNARD <colin.bernard@ymail.com>
2023-01-14 13:38:10 +0100

    compare_images method ok

---
+++ algo-tester/const.py
@@ -0,0 +1,19 @@
+DATASETS: list[str] = [
+    "Bootstrap",
+    "Camouflage",
+    "ForegroundAperture",
+    "LightSwitch",
+    "MovedObject",
+    "TimeOfDay",
+    "WavingTrees"
+]
+
+ALGORITHMS: list[str] = [
+    "IndependantMultimodal",
+    "LBFuzzyGaussian",
+    "LBMixtureOfGaussians",
+    "LBSimpleGaussian",
+    "SigmaDelta",
+    "SuBSENSE",
+    "T2FMRF_UV"
+]
